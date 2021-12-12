374942b64cefb243125f979eee70e41752a39d01
superbear <superbear@users.noreply.github.com>
2021-12-12 23:49:50 +0800

    deploy: superbear/blog@5a4cb01ebcb68cdc21845abdefa401a289e66177

--- netlify.sh
+++ netlify.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # replace domain
-if [ -e "sitemap.xml" ];then
-    sed -i 's#https://superbear.github.io/#https://superbear.netlify.app/#g' sitemap.xml
-fi
+[ -e "sitemap.xml" ] && sed -i 's#https://superbear.github.io/#https://superbear.netlify.app/#g' sitemap.xml
+
+# replace robots.txt
+[ -e "robots.netlify.txt" ] && mv robots.netlify.txt robots.txt
